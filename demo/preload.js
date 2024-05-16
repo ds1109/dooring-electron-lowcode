88208f0c9a9b32cc7d022df4311b545b3a591f88
dingshun <1223469134@qq.com>
2024-05-16 15:46:37 +0800

    dome

---
+++ demo/preload.js
@@ -0,0 +1,13 @@
+const { contextBridge, ipcRenderer } = require('electron')
+
+// contextBridge.exposeInMainWorld('versions', {
+//   node: () => process.versions.node,
+//   chrome: () => process.versions.chrome,
+//   electron: () => process.versions.electron,
+//   ping: () => ipcRenderer.invoke('ping')
+//   // 除函数之外，我们也可以暴露变量
+// })
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  setTitle: (title) => ipcRenderer.send('set-title', title)
+})
